04355c70f4c60cfa38d42f94b667245d6e012dee
Taciano Morais Silva <tacianosilva@gmail.com>
2015-08-26 16:55:25 -0300

    Adição da extração de anotações de atributos #15
    #15: Adicionar suporte à anotações de atributos 
    
    Task-Url: http://github.com/joaoarthurbm/designwizard/issues/issue/15
    
    A classe FactsExtractionClassVisitor agora retorna um
    FactsExtractionFieldVisitor (antes retornava um EmptyVisitor). Na classe
    FactsExtractionFieldVisitor agora são extraídas informações da anotação
    e da relação com o atributo.

--- src/org/designwizard/extractor/asm/visitor/FactsExtractionFieldVisitor.java
+++ src/org/designwizard/extractor/asm/visitor/FactsExtractionFieldVisitor.java
@@ -1,8 +1,34 @@
 package org.designwizard.extractor.asm.visitor;
 
+import org.designwizard.extractor.asm.event.FactEvent;
 import org.designwizard.extractor.asm.event.FactsEventSourceImpl;
+import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.FieldVisitor;
 
 public class FactsExtractionFieldVisitor extends FactsEventSourceImpl implements FieldVisitor {
-// TODO nothing for a while.
+	
+	private String fieldName;
+
+	public FactsExtractionFieldVisitor(String fieldName) {
+		this.fieldName = fieldName;
+	}
+	
+	@Override
+	public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+		this.fieldName = name;
+		super.visit(version, access, name, signature, superName, interfaces);
+	}
+
+	@Override
+	public AnnotationVisitor visitAnnotation(String annotationName, boolean isVisible) {
+		// Cria um novo FactEvent para Annotations do FieldNode
+		super.factEvent = new FactEvent(FactsExtractionFieldVisitor.class, annotationName, isVisible);
+		super.fireAnnotationExtracted();
+
+		// Caller = classname and Called = desc (annotation)
+		super.factEvent = new FactEvent(FactsExtractionFieldVisitor.class, "ISANNOTATEDBY", this.fieldName, annotationName);
+		super.fireRelationExtracted();
+		
+		return super.visitAnnotation(annotationName, isVisible);
+	}
 }
