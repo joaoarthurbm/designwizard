d39d3d0b70d940e13c044ef10417e830f40bcb1d
Taciano Morais Silva <tacianosilva@gmail.com>
2016-04-02 13:14:21 -0300

    Annotated classes for use in unit tests issue #39.

---
+++ src_tests/tests/org/designwizard/design/mocks/annotations/issue39/AnnotatedObjectB.java
@@ -0,0 +1,8 @@
+package tests.org.designwizard.design.mocks.annotations.issue39;
+
+import tests.org.designwizard.design.mocks.annotations.external.AnnotationB;
+
+@AnnotationB
+public class AnnotatedObjectB {
+
+}
---
+++ src_tests/tests/org/designwizard/design/mocks/annotations/issue39/AnnotatedObjectF.java
@@ -0,0 +1,6 @@
+package tests.org.designwizard.design.mocks.annotations.issue39;
+
+@AnnotationF
+public class AnnotatedObjectF {
+
+}
